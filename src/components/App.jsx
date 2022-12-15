561d43eacea5223f40a97fd6799989ab57203e2e
olegtremba1234 <olegtremba@gmail.com>
2022-12-15 20:33:43 +0200

    commit

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      Слава Україні!
+    </div>
+  );
+};
+
+
+// API_KEY= 4b8864cf5207c2132295f1e95f786e7c
